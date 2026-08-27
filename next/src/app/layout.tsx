14c0704ee0826b318a392e10b463b53e2a208c6a
EffessDev <faseeh1080@gmail.com>
2026-08-27 16:14:05 +0530

    add styles and icons

---
+++ next/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "EffessDev",
+  description:
+    "I build apps, games, and websites, and program MCUs and IoT devices.",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
